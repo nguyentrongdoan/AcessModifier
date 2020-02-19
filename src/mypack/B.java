a800722adc7c97fabe870e45a27e663a255c44c8
nguyentrongdoan <nguyendoan206@gmail.com>
2020-02-19 16:09:47 +0700

    Acess Modifier

---
+++ src/mypack/B.java
@@ -0,0 +1,24 @@
+package mypack;
+import pack.*;
+//public class B {
+//    public static void main(String[] args) {
+//        A obj = new A();
+//        obj.msg();
+//    }
+//}
+
+// Voi protected:
+//class B extends  A{
+////    public static void main(String[] args) {
+////        B obj = new B();
+////        obj.msg();
+////    }
+////}
+
+// Voi public:
+class B{
+    public static void main(String[] args) {
+        A obj = new A();
+        obj.msg();
+    }
+}
---
+++ src/pack/A.java
@@ -0,0 +1,24 @@
+package pack;
+
+public class A {
+//   private int data = 40;
+//    private void msg(){
+//        System.out.println("Hello java");
+
+// Voi protected :
+//    protected void msg(){
+//        System.out.println("Hello");
+//    }
+
+    // Voi public.
+    public void msg(){
+        System.out.println("Hello");
+    }
+}
+//public class Simple{
+//    public static void main(String[] args) {
+//        A ojb = new A();
+//        System.out.println(ojb.data);
+//        obj.msg();
+//    }
+//}
